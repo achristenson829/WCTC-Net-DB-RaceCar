63de75e033d25cedbf6a9ec8a03a208f256d299c
achristenson829 <achristenson829@gmail.com>
2022-03-07 23:41:42 -0600

    FordTruck and BillyBob Classes

---
+++ RaceTrack/Cars/FordTruck.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RaceTrack.RaceTrack.Cars
+{
+    public class FordTruck : RaceCar
+    { 
+        public FordTruck()
+        {
+            Name = "Ford Truck";
+            TopSpeed = 75;
+        }
+
+        public override void StartEngine()
+        {
+            Console.WriteLine($"The {Name} starts with a Vroom!");
+        }
+
+        public virtual void Brake()
+        {
+            Console.WriteLine($"The {Name} sputters and falls to {Position}");
+        }
+    }
+}
---
+++ RaceTrack/Drivers/BillyBob.cs
@@ -0,0 +1,18 @@
+﻿using RaceTrack.RaceTrack.Cars;
+
+namespace RaceTrack.RaceTrack.Drivers
+{
+    public class BillyBob : Driver
+    {
+        public BillyBob(RaceCar car) : base(car)
+        {
+            Name = "Billy Bob";
+            SkillLevel = 6;
+        }
+
+        public override void Drive()
+        {
+            Car.Accelerate(SkillLevel);
+        }
+    }
+}
